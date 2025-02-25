9b44ef2923e80ba4b4848a8d4bffdf0612bd110a
SingletonRcc <singletonrcc@gmail.com>
2025-02-25 23:50:43 +0000

    Add react-icons dependency and update Home component layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,44 @@
+import { FaTrophy, FaBowlFood } from "react-icons/fa6";
+import { FaHeartbeat } from "react-icons/fa";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full h-full bg-backdrop p-20 cursor-default">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-[10rem] text-primarycol font-extrabold">EmpowerAll</h1>
+        <h2 className="relative left-20 text-[5rem] font-bold text-secondarycol">Health and welbeing made for all</h2>
+        <div className="grid grid-cols-2 mt-40 gap-2">
+          <div className=" bg-secondarycol p-20 rounded-3xl flex self-center">
+            <p className="text-5xl font-semibold text-backdrop">
+              At EmpowerAll, we believe that fitness is more than just a workout—it's a path
+              to transforming your mind, body, and life. Whether you're a beginner or an 
+              experienced athlete, our goal is to help you unlock your true strength.
+              It's time to challenge yourself and redefine what's possible with EmpowerAll.
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="w-full h-full bg-accentcol rounded-3xl container flex text-[120px] text-primarycol justify-around flex-row py-10">
+              <button className="hover:scale-150 duration-500"><FaHeartbeat/></button>
+              <button className="hover:scale-150 duration-500"><FaBowlFood/></button>
+              <button className="hover:scale-150 duration-500"><FaTrophy/></button>
+            </div>
+            <div className="flex justify-center bg-primarycol text-secondarycol rounded-3xl h-full items-center">
+              <p className="text-5xl p-4 font-bold">
+                Join the wellbeing revoultion
+              </p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+        <Image 
+                src="/dumbell.png" 
+                alt="Meal Planner" 
+                className="w-full h-full "
+                width={0}
+                height={0}/>
+        </div>
+      </div>
     </div>
   );
 }
